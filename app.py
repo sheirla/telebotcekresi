69bb983e8bb7a8376a8ada2cdc8f49ba3a2c5773
wanxxun <50426973+wanxxun@users.noreply.github.com>
2021-01-12 15:50:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -108,30 +108,8 @@ def starttiktok(message):
     bot.register_next_step_handler(sent3, tiktokstarted)
 
 def tiktokstarted(message):
-    tiktokurl = (message.text)
-    
-    payloadtiktok='url='+tiktokurl+'&token=2fd0de11cb3f0cf7d507ee33869e70709a3fd428744d8b5730d003776db98ebf'
-    headerstiktok = {
-    'cookie': '__cfduid=d33d3b6499977fe797170dbc8ec5309431610126208; __cflb=0H28vNadMLAyXKUf1Fe7hcf2dQRRDpKqwQZqnTjqcom; __gads=ID=4e6278d305e8c0b2-22ed10d597c5005b:T=1610126209:RT=1610126209:S=ALNI_MbX5ZJ6GMrDdCIE9LCgkcWSMSO6Bg; current_language=ID; PHPSESSID=vo1km7l3o55ira5ko14d21b3io; _gid=GA1.2.172905107.1610434680; __atssc=google%3B2; _gat=1; _gat_gtag_UA_162798444_8=1; _ga_2KT415JS53=GS1.1.1610434679.2.1.1610435206.60; _ga=GA1.1.188138072.1610126209; __atuvc=1%7C1%2C2%7C2; __atuvs=5ffd48781d044837001; __cflb=025XBJYGdEpTJNVqnGV9cshdcjdDhTekAYgBF19Urk735WnTwHVtXSsdFVkTJdpdqdXcKXD1tyUhr98KZBW4xT; current_language=ID',
-    'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    responsetiktok = requests.request("POST", urltiktok, headers=headerstiktok, data=payloadtiktok)
-
-    datatiktok = responsetiktok.text
-      
-    soup = BeautifulSoup(datatiktok, "html.parser")
-    datavideomentah = soup.find("a",{"class":"abutton is-success is-fullwidth"})
-    datavideojadi = datavideomentah['href']
-    print(datavideojadi)
-    req = Request(str(datavideojadi), headers={'User-Agent': 'Mozilla/5.0'})
-    
-    f = open('out.mp4','wb')
-    f.write(urlopen(req).read())
-    f.close()
-    bot.send_chat_action(message.chat.id, 'upload_video')
-    img = open('out.mp4', 'rb')
-    bot.send_video(message.chat.id, img, reply_to_message_id=message.message_id)
-    img.close()
+def send_welcome(message):
+	bot.reply_to(message, "Sedang dalam perbaikan")
 
 
 bot.polling(none_stop=True)
