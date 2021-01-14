6f711c4432882b003fd7249122b2466d8de2af52
wanxxun <50426973+wanxxun@users.noreply.github.com>
2021-01-14 07:42:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 import requests
 from urllib.request import Request, urlopen
+import urllib.parse as urlparse
+from urllib.parse import parse_qs
 from bs4 import BeautifulSoup
 
 urlcekongkir = "https://pluginongkoskirim.com/cek-tarif-ongkir/front/resi-amp?__amp_source_origin=https%3A%2F%2Fpluginongkoskirim.com"
@@ -92,13 +94,63 @@ def youtubestarted(message):
       
     vide_urlyt = datayoutube['data'][0]['video']['url']
     req = Request(vide_urlyt, headers={'User-Agent': 'Mozilla/5.0'})
+    bot.send_message(message.chat.id, 'Video sedang di upload kepada klean..')
     f = open('out.mp4','wb')
     f.write(urlopen(req).read())
     f.close()
     bot.send_chat_action(message.chat.id, 'upload_video')
     img = open('out.mp4', 'rb')
     bot.send_video(message.chat.id, img, reply_to_message_id=message.message_id)
+    bot.send_message(message.chat.id, 'Video selesai terupload kepada klean..')
     img.close()
+
+""" ================================================================================================== """
+
+@bot.message_handler(commands=['ytmp3'])
+def startyoutubemp3(message):
+    global youtubemp3url
+    sent4 = bot.send_message(message.chat.id, 'Input URL')
+    bot.register_next_step_handler(sent4, youtubemp3started)
+
+def youtubemp3started(message):
+    youtubemp3url = (message.text)
+    
+    parsed = urlparse.urlparse(youtubemp3url)
+    checkurl = parse_qs(parsed.query).get('v')
+    if checkurl is None:
+        x = parsed.path
+        ytquery = x.replace("/", "")
+    else:
+        ytquery = parse_qs(parsed.query).get('v')[0]
+    
+    print(ytquery)
+    ytmp3url = "https://api.youtube-mp3.org.in/@audio/"+ytquery+"/?title="+ytquery
+    headersytmp3 = {
+    'Content-Type': 'application/json'
+    }
+    responseyoutubemp3 = requests.request("GET", ytmp3url, headers=headersytmp3)
+    print(responseyoutubemp3)
+    datayoutubemp3 = responseyoutubemp3.json()
+      
+    vide_urlytmp3 = datayoutubemp3['url']
+    bot.send_message(message.chat.id, 'Klik Download untuk mengunduh Manual', reply_to_message_id=message.message_id,  reply_markup=InlineKeyboardMarkup([
+        [InlineKeyboardButton(text='Download mp3', url=''+vide_urlytmp3)],
+    ]))
+
+    req = Request(vide_urlytmp3, headers={'User-Agent': 'Mozilla/5.0'})
+    bot.send_message(message.chat.id, 'Audio sedang di upload kepada klean..')
+    f = open('out.mp3','wb')
+    f.write(urlopen(req).read())
+    f.close()
+    bot.send_chat_action(message.chat.id, 'upload_audio')
+    mp3 = open('out.mp3', 'rb')
+    bot.send_audio(message.chat.id, mp3, reply_to_message_id=message.message_id)
+    bot.send_message(message.chat.id, 'Audio selesai terupload kepada klean..')
+    mp3.close()
+
+ 
+
+""" ================================================================================================== """
 
 """ ================================================================================================== """
 @bot.message_handler(commands=['tiktok'])
